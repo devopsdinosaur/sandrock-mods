05416f6b5e976229ebcd72b70d66927f620bc61d
devopsdinosaur <devopsdinosaur@gmail.com>
2024-02-01 00:02:42 -0500

    ---

---
+++ testing/TestingPlugin.cs
@@ -0,0 +1,80 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+
+[BepInPlugin("devopsdinosaur.sunhaven.testing", "Testing", "0.0.1")]
+public class ActionSpeedPlugin : BaseUnityPlugin {
+
+	private Harmony m_harmony = new Harmony("devopsdinosaur.sunhaven.testing");
+	public static ManualLogSource logger;
+	private static ConfigEntry<bool> m_enabled;
+	
+	private void Awake() {
+		logger = this.Logger;
+		try {
+			m_enabled = this.Config.Bind<bool>("General", "Enabled", true, "Set to false to disable this mod.");
+			if (m_enabled.Value) {
+				this.m_harmony.PatchAll();
+			}
+			logger.LogInfo("devopsdinosaur.sunhaven.testing v0.0.1" + (m_enabled.Value ? "" : " [inactive; disabled in config]") + " loaded.");
+		} catch (Exception e) {
+			logger.LogError("** Awake FATAL - " + e);
+		}
+	}
+
+	public static bool list_descendants(Transform parent, Func<Transform, bool> callback, int indent) {
+		Transform child;
+		string indent_string = "";
+		for (int counter = 0; counter < indent; counter++) {
+			indent_string += " => ";
+		}
+		for (int index = 0; index < parent.childCount; index++) {
+			child = parent.GetChild(index);
+			logger.LogInfo(indent_string + child.gameObject.name);
+			if (callback != null) {
+				if (callback(child) == false) {
+					return false;
+				}
+			}
+			list_descendants(child, callback, indent + 1);
+		}
+		return true;
+	}
+
+	public static bool enum_descendants(Transform parent, Func<Transform, bool> callback) {
+		Transform child;
+		for (int index = 0; index < parent.childCount; index++) {
+			child = parent.GetChild(index);
+			if (callback != null) {
+				if (callback(child) == false) {
+					return false;
+				}
+			}
+			enum_descendants(child, callback);
+		}
+		return true;
+	}
+
+	public static void list_component_types(Transform obj) {
+		foreach (Component component in obj.GetComponents<Component>()) {
+			logger.LogInfo(component.GetType().ToString());
+		}
+	}
+
+	[HarmonyPatch(typeof(Pathea.AnimalCardFight.AnimalCardFightModule), "BeginGame")]
+	class HarmonyPatch_AnimalCardFightModule_BeginGame {
+
+		private static void Postfix(Pathea.AnimalCardFight.AnimalCardFightModule __instance) {
+			__instance.GetType().GetField("fadorTotal", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(__instance, 13);
+			__instance.EndGame();
+		}
+	}
+}
